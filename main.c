b7fe202624613654351575fd152b8e99fc277aa6
David Jones <drj@pobox.com>
2014-03-07 21:16:08 +0000

    PWM FTW!

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 /*
     Continuity.
 
-    Piezo wired between PB2 and GND.
+    Piezo wired between PB0 and GND.
 
-    Shorting PB0 and GND will make Piezo beep.
+    Shorting PB2 and GND will make Piezo beep.
+    Piezo waveform is controlled by Timer 0, and
+    it is gated on or off using the DDRB register.
+
+    REFERENCES
+
+    [ATtinyDS] Atmel ATtiny 25/45/85 Datasheet
 */
 
 #include <stdint.h>
@@ -26,6 +32,10 @@ These are SRAM addresses, recall SRAM address = IO address + 0x20.
 #define DDRB (0x37)
 #define PINB (0x36)
 
+#define TCCR0B (*(volatile uint8_t *)(0x53))
+#define TCCR0A (*(volatile uint8_t *)(0x4A))
+#define OCR0A (*(volatile uint8_t *)(0x49))
+
 void
 delay(uint16_t i) {
   do {
@@ -42,15 +52,24 @@ int
 main(void) {
   unsigned char i;
 
-  BIT_SET(DDRB, 2);
-  BIT_SET(PORTB, 0); /* pull-up */
+  BIT_SET(PORTB, 2); /* pull-up */
+
+  /* [ATtinyDS] 11.9 refers */
+  /* CTC, Toggle ouput. */
+  TCCR0A = 0x42;
+  /* (61 + 1) * prescale * 2 = 7936 clocks per period
+   * which is 1008Hz at a clock of 8MHz. */
+  OCR0A = 61;
+  /* Set T0 clock to flck/64 */
+  TCCR0B = 0x3;
 
   while(1) {
-    if(!BIT_GET(PINB, 0)) {
-      BIT_SET(PORTB, 2);
-      delay(1);
-      BIT_CLR(PORTB, 2);
-      delay(1);
+    if(!BIT_GET(PINB, 2)) {
+      /* OC0A */
+      BIT_SET(DDRB, 0);
+      delay(5);
+    } else {
+      BIT_CLR(DDRB, 0);
     }
   }
 }
